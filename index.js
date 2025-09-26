c090dbc5624fd24afec76ab2b2d75ab96367d820
ToanTiles <descantoine7@gmail.com>
2025-09-25 21:56:56 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// api/index.js
-export default function handler(req, res) {
-  res.setHeader('X-Frame-Options', 'ALLOWALL');
-  res.setHeader('Content-Security-Policy', 'frame-ancestors *;');
-
-  // send your HTML file
-  res.sendFile('index.html', { root: process.cwd() });
-}
